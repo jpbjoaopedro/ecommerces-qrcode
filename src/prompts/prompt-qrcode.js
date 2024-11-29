be5c9184cc5d92b948b8495bf0b4ddedb3a2700b
Joao Pedro <jbandeira@detro.rj.gov.br>
2024-11-29 10:37:24 -0300

    Add QR code prompt configuration with validation and descriptions

---
+++ src/prompts/prompt-qrcode.js
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+const promptQRCode = [
+    {
+        name: "link",
+        description: chalk.yellow("Digite o link para gerar o QRCode"),
+    },
+    {
+        name: "type",
+        description: chalk.yellow("Escolha o tipo de QRCode (1 - Normal) ou (2 - Terminal)"),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Escolha entre 1 ou 2"),
+        required: true,
+    }
+]
+
+export default promptQRCode;
