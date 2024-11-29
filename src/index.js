71e2e83a83ffebd0c9d270b4260f181694eaf368
Joao Pedro <jbandeira@detro.rj.gov.br>
2024-11-29 13:18:31 -0300

    Add QR code generation functionality with prompt integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import prompt from 'prompt';
 import mainPrompt from './prompts/prompt-main.js';
+import createQRCode from './services/qr-code/create.js';
 
 async function main() {
     prompt.get(mainPrompt, async function (err, choose) {
-        if (choose.select == 1) console.log('QR CODE escolhido');
+        if (err) {
+            console.log('Erro na aplicação');
+            return;
+        }
+        if (choose.select == 1) createQRCode();
         if (choose.select == 2) console.log('PASSWORD escolhido');
     });
+
+    prompt.start();
 };
 
 main();
